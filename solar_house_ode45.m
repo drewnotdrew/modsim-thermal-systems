2adb064a3a1ad4cb789e6d781de8c37ca69f992d
kstellmacher <kstellmacher575@gmail.com>
2021-10-28 17:25:37 -0400

    update

--- solar_house_ode45.m
+++ solar_house_ode45.m
@@ -1,26 +1,31 @@
-function [T,D] = solar_house_ode45()
+function [T,D] = solar_house_ode45() %T = temperature D = day
 
 %parameters
-k(1) = 300; %house int starting temperature (K)
-k_e = 290; %external temperature (K)
+t(1) = 300; %house int starting temperature (K)
+t_e = 290; %external temperature (K)
 w = 10; %sidelength of house (m)
 wt = .1; %thickness of walls (m)
 
-SA = w^2*5; %surface area of house (m)
+SA = w^2*5; %surface area of house (m^2)
+V = w^3; %volume of house(m^3)
 
+h_a = 10; %heat transfer of air
+h_w = 500;%heat transfer of walls
 
+d_0 = 0;
+d_end = 60; %in days
 
-t_0 = 0;
-t_end = 30*60; %in seconds
+t_0 = 300; %house interior starting temperature (K)
 
-k_0 = 370; %interior starting temperature (K)
+[T,D] = ode45(@rate_func,[d_0, d_end],[t_0]);
 
-[t, M] = ode45(@rate_func,[t_0, t_end],[C_0]);
-
-    function res = rate_func (~,M) 
-        dQdt = -(k*SA/c_t)*(M(1)-k_e); %formula for conductive cooling (Energy)
-        dTdt = dQdt/(V*d*h); %converting energy to change in temperature
-        res = dTdt;
+    function res = rate_func (~,D) 
+        c_d = k*SA/c_t*(D(1)-k_e); %calculating conduction
+        c_vr = 1/(h_a*A) + 1/(h_c*A); %calculating convective resistance
+        c_v = (1/c_vr) * (D(1)-k_e); %calculating conduction
+        dTdt = c_u + c_v;
+        C_c = dTdt/(V*d*h); %converting energy to change in temperature
+        res = -C_c; %calculating net flow
     end
 
 
