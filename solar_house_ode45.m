bb95a31d678c47127d811b64cc5183594b2c593b
kstellmacher <kstellmacher575@gmail.com>
2021-10-30 19:17:19 -0400

    Update solar_house_ode45.m

--- solar_house_ode45.m
+++ solar_house_ode45.m
@@ -1,20 +1,20 @@
 function [T,D] = solar_house_ode45() %T = temperature D = day
 
 %parameters
-Tf(1) = 290; %floor starting temperature (K)
-Tr(1) = 290; %house int starting temperature (K)
-Ta = 284; %external temperature (K)
+Tf(1) = 293; %floor starting temperature (K)
+Tr(1) = 293; %house int starting temperature (K)
+Ta = 263.56; %external temperature (K)
 w = 10; %sidelength of house (m)
 wt = .15; %thickness of walls (m)
 
 A = w^2; %area of floor (m)
 SA = w^2*5; %surface area of house (m^2)
 V = w^3; %volume of house(m^3)
-I = 100; %insulation of walls
+I = 1.3; %insulation of walls (material:brick)
 
-e = 1; %efficiency of floor
+e = .885; %efficiency of floor (emmisivity of oak)
 h_a = .1; %heat transfer of air
-h_w = .8; %heat transfer of walls
+h_w = .8; %heat transfer of walls (brick)
 h_f = .45; %heat coefficient of floor
 d = .0001;%density of air (kg/m^3)
 
@@ -27,7 +27,7 @@ Tf_0 = 300; %floor starting temperature (K)
 
 [T,D] = ode45(@rate_func,t_span,Tf_0,Tr_0);
 
-    function res = rate_func (~,T) 
+    function res = rate_func (~,D) 
         
         dUfdt = e*I*A - h_f*A*(Tf - Tr); %change in energy in the floor
         dUrdt = h_f*A*(Tf - Tr) - ((h_w*SA)/wt)*(Tr - Ta);%change in energy in the room
