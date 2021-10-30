213b06ced012140ea9e459be8b649254038e8f48
kstellmacher <kstellmacher575@gmail.com>
2021-10-30 19:40:40 -0400

    update

--- solar_house_ode45.m
+++ solar_house_ode45.m
@@ -8,18 +8,19 @@ w = 10; %sidelength of house (m)
 wt = .15; %thickness of walls (m)
 
 A = w^2; %area of floor (m)
+PA = A*sind(68); %projected area (m^2)
 SA = w^2*5; %surface area of house (m^2)
 V = w^3; %volume of house(m^3)
-I = 1.3; %insulation of walls (material:brick)
+I = 170.14; %incident solar radiation (w/m^2)
 
 e = .885; %efficiency of floor (emmisivity of oak)
 h_a = .1; %heat transfer of air
 h_w = .8; %heat transfer of walls (brick)
 h_f = .45; %heat coefficient of floor
-d = .0001;%density of air (kg/m^3)
+d = 1.293;%density of air (kg/m^3)
 
 d_0 = 0;
-d_end = 60; %in days
+d_end = 5; %in days
 t_span = [d_0,d_end];
 
 Tr_0 = 300; %house interior starting temperature (K)
@@ -29,7 +30,7 @@ Tf_0 = 300; %floor starting temperature (K)
 
     function res = rate_func (~,D) 
         
-        dUfdt = e*I*A - h_f*A*(Tf - Tr); %change in energy in the floor
+        dUfdt = e*I*PA - h_f*A*(Tf - Tr); %change in energy in the floor
         dUrdt = h_f*A*(Tf - Tr) - ((h_w*SA)/wt)*(Tr - Ta);%change in energy in the room
         T = dUrdt/(V*d*h_a); %converting energy to change in temperature
         res = -T; 
