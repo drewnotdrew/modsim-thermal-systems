446f0230026cc7b9bf29f7d108025ac9a256884f
drewnotdrew <53927275+drewnotdrew@users.noreply.github.com>
2021-11-03 02:22:37 -0400

    Cleaned up code
    Normal distribution implementation is next

--- solar_house_day_night.m
+++ solar_house_day_night.m
@@ -1,27 +1,37 @@
-function [Tt,Dt] =  solar_house_day_night();
+function [Tt,Dt] =  solar_house_day_night(wt);
+    % Initialize T, D, Tt, Dt, d_0, d_end
     T = [];
-    D = [];
     Tt = [];
     Dt = [];
     d_0 = 0;
     d_end = 0;
     D = [293, 293];
-    for i = 1:60
+    
+    days = 10; % Number if days to run the simulation
+    
+    %mu = 170.14;
+    %sigma = (5/4)*3600;
+    %x = 1;
+    %p = normcdf(x,mu,sigma)
+    
+    for i = 1:days*2;
         d_0 = d_end;
-        if mod(i,2) == 1;
-           I = 170.14; %incident solar radiation (w/m^2)
-           d_end = d_end + 36000;
-        elseif mod(i,2) == 0;
-           I = 0; %incident solar radiation (w/m^2)
-           d_end = d_end + 50400;
+        if mod(i,2) == 1; % For the first 10 hours of the day, set the incident solar radiation for daytime
+           I = 170.14; %incident day solar radiation (w/m^2)
+           d_end = d_end + 36000; % Index end of day
+        elseif mod(i,2) == 0; % For the last 14 hours of the day, set the incident solar radiation for nighttime
+           I = 0; %incident night solar radiation (w/m^2)
+           d_end = d_end + 50400; % Index end of day
         end
-        t_span = [d_0,d_end];
-        %t_span
-        %d_end = 60*60*24*(5/12); %in seconds
-        [T,D] = solar_house_ode45(I, t_span, D);
-        Tt = [Tt; T];
+        
+        t_span = [d_0,d_end]; % Set the time span to run the simulation during day or night
+  
+        [T,D] = solar_house_ode45(I, t_span, D, wt); % Run the simulation
+        
+        Tt = [Tt; T]; % Collect data from the simulation and concatenate it into total matricies, Tt and Dt 
         Dt = [Dt; D];
+        
     end
-    Tt = Tt - 273;
+    Tt = Tt - 273; % Convert temperatures from K to C
     Dt = Dt - 273;
 end
--- solar_house_ode45.m
+++ solar_house_ode45.m
@@ -1,72 +1,51 @@
-function [T,D] = solar_house_ode45(I, t_span, D) %T = temperature D = day
+function [T,D] = solar_house_ode45(I, t_span, D, wt) %T = temperature D = day
 
-%parameters
-% Tf(1) = 293; %floor starting temperature (K)
-% Tr(1) = 293; %house int starting temperature (K)
-Ta = 263.56; %external temperature (K)
-w = 10; %sidelength of house (m)
-wt = .30; %thickness of walls (m)
-wf = .05715; %thickness of floor (m)
+% Model Parameters
+Ta = 263.56; % external temperature (K)
+w = 10; % sidelength of house (m)
+wf = .05715; % thickness of floor (m)
 
-A = w^2; %area of floor (m)
-PA = A*sind(68); %projected area (m^2)**
-SA = w^2*5; %surface area of house (m^2)
-V = w^3; %volume of house(m^3)
-V_f = wf*w*w; %volume of floor
+A = w^2; % area of floor (m)
+S = 68; % Angle between incident sun rays and the floor
+PA = A*sind(S); % projected area (m^2)**
+SA = w^2*5; % surface area of house (m^2)
+V = w^3; % volume of house(m^3)
+V_f = wf*w*w; % volume of floor
 
-e = 0.55; %efficiency of floor (emmisivity of oak)
-h_a = 0.026; %thermal conductivity of air
-h_w = 0.05; %thermal conductivity of walls (insulation)
-h_f = 15; %thermal conductivity of floor (wood)********
-s_f = 1850; %specific heat of floor(j/kg C)
-s_a = 1005; %specific heat of air (j/kg C)
-d = 1.293;%density of air (kg/m^3)
-d_f = 745; %density of floor (kg/m^3)
-m_a = V*d; %mass of air k(g)
-m_f = d_f*V_f; %mass of floor (kg)
+e = 0.5; % efficiency of floor (emmisivity of oak)
+h_a = 0.026; % thermal conductivity of air
+h_w = 0.05; % thermal conductivity of walls (insulation)
+h = 15; % Convection heat coefficient of air
+s_f = 1850; % specific heat of floor(j/kg C)
+s_a = 1005; % specific heat of air (j/kg C)
+d = 1.293;% density of air (kg/m^3)
+d_f = 745; % density of floor (kg/m^3)
+m_a = V*d; % mass of air k(g)
+m_f = d_f*V_f; % mass of floor (kg)
 
+Ds = size(D, 1);
 
-
-%Tf_0 = 293; %floor starting temperature (K)
-%Tr_0 = 293; %house interior starting temperature (K)
-Ds = size(D, 1)
-
-Tf_0 = D(Ds, 1)
-Tr_0 = D(Ds, 2)
+Tf_0 = D(Ds, 1);
+Tr_0 = D(Ds, 2);
 
  Uf = temperatureToEnergy(Tf_0, m_f, s_f);
  Ur = temperatureToEnergy(Tr_0, m_a, s_a);
 init = [Uf, Ur]; % initial values
 [T,D] = ode45(@rate_func,t_span, init);
-    function res = rate_func (~,D)
+    function res = rate_func (~,D);
                   Tf = D(2)/(V_f*d_f*s_f); %converting energy to change in temperature 
                   Tr = D(1)/(V*d*s_a); %converting energy to change in temperature
         
         Tf = energyToTemperature(D(1), m_f, s_f);
         Tr = energyToTemperature(D(2), m_a, s_a);
-        %Uf = temperatureToEnergy(D(1), m_f, s_f);
-        %Ur = temperatureToEnergy(D(2), m_a, s_a);
-        Test = e*I*PA;% Delete
-%         dUfdt = 0;
-%         dUrdt = 0;
-% 
-%         tf_minus_tr = Tf-Tr
-%         tr_minus_ta = Tr-Ta
-%         room_heat_to_outside = -((h_w*SA)/wt)*(Tr - Ta)
 
-        flowin = e*I*PA;
-        flowout = h_f*A*(Tf - Tr);
-        dUfdt = e*I*PA - h_f*A*(Tf - Tr); %change in energy in the floor
-        dUrdt = h_f*A*(Tf - Tr) - ((h_w*SA)/wt)*(Tr - Ta);%change in energy in the room
-        
-%         dTfdt = energyToTemperature(dUrdt, m_a, h_a);
-%         dTrdt = energyToTemperature(dUfdt, m_f, h_f);
+        dUfdt = e*I*PA - h*A*(Tf - Tr); %change in energy in the floor
+        dUrdt = h*A*(Tf - Tr) - ((h_w*SA)/wt)*(Tr - Ta);%change in energy in the room
         
         res = [dUfdt; dUrdt];
     end
 
 D(:,1) = energyToTemperature(D(:,1), m_f, s_f);
 D(:,2) = energyToTemperature(D(:,2), m_a, s_a);
-%D = D - 273;
-%D = [Tf,Tr]; 
+
 end
